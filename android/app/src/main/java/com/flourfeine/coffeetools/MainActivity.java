e883238a7c66b7a1010ba3f41ef0e2cc8c2ed1b2
Anuj Kilje <ajkilje@gmail.com>
2026-07-28 18:37:33 +0530

    feat: add Android Capacitor project

---
+++ android/app/src/main/java/com/flourfeine/coffeetools/MainActivity.java
@@ -0,0 +1,5 @@
+package com.flourfeine.coffeetools;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flourfeine.coffeetools',
+  appName: 'Coffee Tools',
+  webDir: 'dist'
+};
+
+export default config;
